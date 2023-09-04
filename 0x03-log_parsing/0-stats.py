65e877ae81c84d3c872264079f56d04d9036290f
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-09-04 13:00:32 +0300

    init log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,49 @@
 #!/usr/bin/python3
-
+"""Log Parsing Interview Question"""
 import sys
-import signal
 
-# Initialize variables to store metrics
-total_file_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
 
-def print_statistics():
-    global total_file_size
-    global status_code_counts
+def main():
+    """ Main function """
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
-    print(f"File size: {total_file_size}")
-    for code, count in sorted(status_code_counts.items()):
-        if count > 0:
-            print(f"{code}: {count}")
-    print()
+    def print_status():
+        """ Prints status """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
 
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-try:
-    for line in sys.stdin:
-        parts = line.strip().split()
-        if len(parts) != 9:  # Corrected to 9, which is the expected number of parts
-            continue
-        
-        _, _, _, _, _, _, status_code, file_size = parts
+    def parse_line(line):
+        """ Checks the line for matches """
         try:
-            status_code = int(status_code)
-            file_size = int(file_size)
-        except ValueError:
-            continue
-        
-        total_file_size += file_size
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-        line_count += 1
-        
-        if line_count % 10 == 0:
-            print_statistics()
+            line = line[:-1]
+            word = line.split(' ')
+            # If the file size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Here the status code comes before file size
+            status_code = int(word[-2])
+            # Moves through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
 
-except KeyboardInterrupt:
-    print_statistics()
-    sys.exit(0)
+    linenum = 1
+    try:
+        for line in sys.stdin:
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_status()
+            linenum += 1
+    except KeyboardInterrupt:
+        print_status()
+        raise
+    print_status()
+
+
+if __name__ == '__main__':
+    main()
