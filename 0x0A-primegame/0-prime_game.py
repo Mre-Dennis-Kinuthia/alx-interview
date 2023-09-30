48bd20ff3f02213e32951a8b522ad4a16892b291
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-09-30 15:02:23 +0300

    init

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,63 +1,45 @@
 #!/usr/bin/python3
-""" Prime Game """
+"""
+Prime Game Interview Question
+"""
 
 
-def isprime(n):
-    """ n: number to check if it is prime"""
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
+def findPrimes(n):
+    """
+    Returns list of primes upto n using the sieve of Eratosthenes algorithm
+    """
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if sieve[i]:
+            for j in range(i*i, n + 1, i):
+                sieve[j] = False
 
-
-def delete_numbers(n, nums):
-    """ delete numbers - assign zero """
-    for i in range(len(nums)):
-        if nums[i] % n == 0:
-            nums[i] = 0
+    primes = [i for i in range(2, n + 1) if sieve[i]]
+    return primes
 
 
 def isWinner(x, nums):
-    """ where x is the number of rounds and nums is an array of n
-        Return: name of the player that won the most rounds
-        Iriaf the winner cannot be determined, return None
-        You can assume n and x will not be larger than 10000
     """
-    nums.sort()
-    winner = False
-    Maria = 0
-    Ben = 0
-    for game in range(x):
-        # print("game# ", game+1)
-        nums2 = list(range(1, nums[game] + 1))
-        # print("nums: ", nums2)
-        turn = 0
-        while True:
-            """
-            # uncomment to monitor turns
-            if turn % 2 != 0:
-                print("Ben turn ")
-            else:
-                print("Maria turn ")
-            """
-            change = False
-            for i, n in enumerate(nums2):
-                # print("n: ", n, "i: ", i)
-                if n > 1 and isprime(n):
-                    delete_numbers(n, nums2)
-                    change = True
-                    turn += 1
-                    break
-            # print("movement: ", nums2)
-            if change is False:
-                break
-        if turn % 2 != 0:
-            Maria += 1
-        else:
-            Ben += 1
-        # print("Maria: {}, Ben: {}".format(Maria, Ben))
-    if Maria == Ben:
+    Solution for prime game
+    """
+    if x <= 0 or not nums:
         return None
-    if Maria > Ben:
-        return "Maria"
-    return "Ben"
+
+    # Player1 = Maria
+    # Player2 = Ben
+    player1 = player2 = 0
+    for i in range(x):
+        if nums[i] == 0:
+            continue
+        counter = len(findPrimes(nums[i]))
+        if (counter % 2) == 0:
+            player2 += 1
+        else:
+            player1 += 1
+
+    if player1 > player2:
+        return 'Maria'
+    elif player2 > player1:
+        return 'Ben'
+    return None
